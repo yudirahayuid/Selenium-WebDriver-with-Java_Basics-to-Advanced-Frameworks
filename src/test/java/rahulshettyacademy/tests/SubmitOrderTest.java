58f6de377dafe33b1b8618509efd14ab32858d3c
yudirahayuid <yudirahayuid@gmail.com>
2024-06-11 19:45:36 +0700

    Bagian 21 belum selesai

--- src/test/java/rahulshettyacademy/tests/SubmitOrderTest.java
+++ src/test/java/rahulshettyacademy/tests/SubmitOrderTest.java
@@ -2,26 +2,27 @@ package rahulshettyacademy.tests;
 
 import org.openqa.selenium.WebElement;
 import org.testng.Assert;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import rahulshettyacademy.TestComponents.BaseTest;
 import rahulshettyacademy.pageobjects.*;
 
-import java.io.IOException;
+import java.util.HashMap;
 import java.util.List;
 
 public class SubmitOrderTest extends BaseTest {
 
     String productName = "ZARA COAT 3";
 
-    @Test
-    public void submitOrder() throws InterruptedException, IOException {
+    @Test(dataProvider = "getData", groups = {"Purchase"})
+    public void submitOrder(HashMap<String, String> input) throws InterruptedException {
 
-        ProductCatalogue productCatalogue = landingPage.loginApplication("yudirahayu321@gmail.com", "Okt0ber8@");
+        ProductCatalogue productCatalogue = landingPage.loginApplication(input.get("email"), input.get("password"));
         List<WebElement> products = productCatalogue.getProductList();
-        productCatalogue.addProductToCart(productName);
+        productCatalogue.addProductToCart(input.get("product"));
         CartPage cartPage = productCatalogue.goToCartPage();
 
-        Boolean match = cartPage.VerifyProductDisplay(productName);
+        Boolean match = cartPage.VerifyProductDisplay(input.get("product"));
         Assert.assertTrue(match);
         CheckoutPage checkoutPage = cartPage.goToCheckout();
         checkoutPage.selectCountry("india");
@@ -36,6 +37,21 @@ public class SubmitOrderTest extends BaseTest {
         ProductCatalogue productCatalogue = landingPage.loginApplication("yudirahayu321@gmail.com", "Okt0ber8@");
         OrderPage orderPage = productCatalogue.goToOrdersPage();
         Assert.assertTrue(orderPage.VerifyOrderDisplay(productName));
+    }
+
+    @DataProvider
+    public Object[][] getData() {
+        HashMap<String, String> map = new HashMap<String, String>();
+        map.put("email", "yudirahayu321@gmail.com");
+        map.put("password", "Okt0ber8@");
+        map.put("product", "ZARA COAT 3");
+
+        HashMap<String, String> map1 = new HashMap<String, String>();
+        map1.put("email", "cobates@gmail.com");
+        map1.put("password", "Okt0ber8@");
+        map1.put("product", "ADIDAS ORIGINAL");
+
+        return new Object[][]{{map}, {map1}};
     }
 
 }
